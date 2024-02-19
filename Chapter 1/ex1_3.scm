b107d2ac94eff1af18f40cf4dbce48a35ac8d7c9
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-02-18 21:41:48 -0800

    Create ex1_3.scm

---
+++ Chapter 1/ex1_3.scm
@@ -0,0 +1,5 @@
+(define (sum-square-two-large a b c)
+  (cond ((and (> a b) (< c b)) (+ (* a a) (* b b)))
+        ((and (> a b) (< b c)) (+ (* a a) (* c c)))
+        ((and (> b a) (< a c)) (+ (* c c) (* b b)))
+        (else (+ (* a a) (* b b)))))
