7b8b8a47360ccb5c0c1a8ade08d50c13b7abd1a3
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2024-02-18 21:51:29 -0800

    Create ex1_4.scm

---
+++ Chapter 1/ex1_4.scm
@@ -0,0 +1,10 @@
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+                   
+#|
+
+The operator + or - is chosen by the if special form.
+This turns the value of b into an absolute value of b.
+Thus the result is to always compute a plus the absolute value of b.
+
+|#
